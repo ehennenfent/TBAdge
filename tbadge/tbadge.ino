e4edbc42401448a2d4436ab9dfb4b781de81ae2a
Eric Hennenfent <ehennenfent@imsa.edu>
2019-04-24 00:05:47 -0500

    Add IO modes 8-15 and 20-27

--- tbadge/tbadge.ino
+++ tbadge/tbadge.ino
@@ -3,6 +3,7 @@
 #include <Adafruit_GFX.h>    // Core graphics library
 #include <Adafruit_ST7735.h> // Hardware-specific library for ST7735
 #include "Adafruit_miniTFTWing.h"
+#include <deque>
 
 Adafruit_miniTFTWing ss;
 Adafruit_ST7735 tft = Adafruit_ST7735(TFT_CS,  TFT_DC, TFT_RST);
@@ -11,20 +12,20 @@ Adafruit_NeoPixel pixels(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
 byte program[MAX_PROGRAM] = {'+', '+', '=', '+', '+', '+', '+', '+', '+', '[', '-', '>', '+', '+', 
                              '+', '+', '+', '+', '+', '+', '<', ']', '>', '+', '?', '+', '?', '+', '?'};
 byte memory[MEMORY_SIZE];
-byte buffer[BUFFER_SIZE];
+std::deque<byte> buffer;
 
 word mpointer;
 word epointer;
 byte io_mode;
 
 
-void render(byte * mem, byte * buf){
+void render(byte * mem, std::deque<byte> &buf){
   for (word i = 0; i < MEMPIXELS; i++){
     pixels.setPixelColor(mem_coord_transform(i), colors[mem[i]]);
   }
 
   for (word i = 0; i < BUFPIXELS; i++){
-    pixels.setPixelColor(buf_coord_transform(i), colors[buf[i]]);
+    pixels.setPixelColor(buf_coord_transform(i), (i < buffer.size()) ? colors[buf[i]] : colors[0]);
   }
 
   pixels.show();
@@ -131,6 +132,7 @@ bool single_step(){
 
 void reset(){
   memset(memory, 0, MEMORY_SIZE);
+  buffer.clear();
   mpointer = 0;
   epointer = 0;
   io_mode = 0;
@@ -140,7 +142,101 @@ void do_io(byte mode){
   switch (mode){
     case 2:
       draw_char(memory[mpointer]);
+      break;
+    case 6:
+      buffer.clear();
+      for (word i = 0; i < MAX_PROGRAM; i++){
+        buffer.push_back(program[i]);
+        if (program[i] == '\x00') break;
+      }
+      break;
+    case 8:
+      if (buffer.size() < BUFFER_SIZE) buffer.push_back(memory[mpointer]);
+      break;
+    case 9:
+      if (!buffer.empty()){
+        memory[mpointer] = buffer.back();
+        buffer.pop_back();
+      }
+      else{
+        memory[mpointer] = 0;
+      }
+      break;
+    case 10:
+      memory[mpointer] = dequeue_fifo();
+      break;
+    case 11:
+      buffer.clear();
+      break;
+    case 12:
+      if (0 <= memory[mpointer] <= 25){
+        memory[mpointer] += 97;
+      }
+      break;
+    case 13:
+      if (0 <= memory[mpointer] <= 25){
+        memory[mpointer] += 65;
+      }
+      break;
+    case 14:
+      if (0 <= memory[mpointer] <= 9){
+        memory[mpointer] += 48;
+      }
+      break;
+    case 15:
+      switch (memory[mpointer]){
+        case 0:
+          memory[mpointer] = '+';
+        case 1:
+          memory[mpointer] = '-';
+        case 2:
+          memory[mpointer] = '<';
+        case 3:
+          memory[mpointer] = '>';
+        case 4:
+          memory[mpointer] = '[';
+        case 5:
+          memory[mpointer] = ']';
+        case 6:
+          memory[mpointer] = '=';
+        case 7:
+          memory[mpointer] = '?';
+        default:
+          break;
+      }
+      break;
+    case 20:
+      memory[mpointer] &= dequeue_fifo();
+      break;
+    case 21:
+      memory[mpointer] |= dequeue_fifo();
+      break;
+    case 22:
+      memory[mpointer] = (dequeue_fifo() ? true : false);
+      break;
+    case 23:
+      memory[mpointer] ^= dequeue_fifo();
+      break;
+    case 24:
+      memory[mpointer] = (byte) mpointer;
+      break;
+    case 25:
+      memory[mpointer] = (byte) (epointer + 1);
+      break;
+    case 26:
+      if ((int) epointer - memory[mpointer] >= 0) epointer -= memory[mpointer];
+      break;
+    case 27:
+      if (epointer + memory[mpointer] < MAX_PROGRAM) epointer += memory[mpointer];
+      break;
   }
+}
+
+byte dequeue_fifo(){
+  if (buffer.empty()) return 0;
+  byte o = buffer.front();
+  buffer.pop_front();
+  return o;
 }
 
 void draw_char(byte c){
